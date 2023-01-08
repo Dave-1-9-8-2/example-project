8efb01a2fcdfcc23a6a2e14b0c48c91c75455e8f
Dave <>
2023-01-08 18:33:06 +0100

    Second commit

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 
 const connection = new WebSocket('ws://example.com/test');
 connection.onopen = (event) => {
-    console.log('Hallo Welt!');
+    console.log('Hallo 2023!, Hallo Welt!');
 };
 connection.onerror = (error) => {
     console.log(`WebSocket Error: ${error}`);
 };
 connection.onclose = (event) => {
     console.log('Verbindung geschlossen');
-    console.log('Hallo Welt!');
+    console.log('Hallo 2023!, Hallo Welt!');
 };
